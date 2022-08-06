44b245a2aa1cec41ce6216febebce173d90a2ff4
Anuj <you@example.com>
2022-08-06 12:20:50 +0530

    logger

---
+++ src/logger/logger.js
@@ -0,0 +1,6 @@
+const name="Anuj"
+let printName=function(){
+    console.log('Welcome to my application.I am ',name,'and a part of FunctionUp Plutonium cohort.')
+}
+module.exports.aab=name
+module.exports.aba=printName
