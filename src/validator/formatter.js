0999ec2ac277f563b3b482c29d01da95e8a2ac3d
AnujTembhurne <you@example.com>
2022-08-06 12:44:37 +0530

    formatter.js

---
+++ src/validator/formatter.js
@@ -0,0 +1,11 @@
+let text="   FunctionUp     "
+let m=text.trim()
+{console.log(m)}
+let n=text.toLowerCase()
+{console.log(n)}
+let o=text.toUpperCase()
+{console.log(o)}
+
+module.exports.m=m
+module.exports.n=n
+module.exports.o=o
