b505043b005c9dd759b6e2f531484bd55e913c0e
AnujTembhurne <you@example.com>
2022-08-17 21:43:29 +0530

    new

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,12 +1,14 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
 
+
 const createBook= async function (req, res) {
     let data= req.body
 
     let savedData= await BookModel.create(data)
     res.send({msg: savedData})
 }
+
 
 const getBooksData= async function (req, res) {
     let allBooks= await BookModel.find( {authorName : "HO" } )
