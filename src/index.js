14eb7c75584ac637622d212905fac919de5843d7
AnujTembhurne <you@example.com>
2022-08-25 13:01:19 +0530

    new

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
 
+const date=require('log-timestamp');
+const ip=require('ip');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,12 +17,12 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
+app.use ( (req, res, next)=> {
+        console.log(ip.address(),req.originalUrl);
         next();
   }
-  );
+  )
+
 
 app.use('/', route);
 
