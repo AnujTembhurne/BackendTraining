76e2f662b8bd41561b5c76f46ce514edc785a104
AnujTembhurne <you@example.com>
2022-08-27 17:52:59 +0530

    new

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -34,8 +34,9 @@ const loginUser = async function (req, res) {
   let token = jwt.sign(
     {
       userId: user._id.toString(),
-      batch: "thorium",
+      batch: "Plutonium",
       organisation: "FunctionUp",
+      month:"August"
     },
     "functionup-plutonium-very-very-secret-key"
   );
@@ -47,17 +48,12 @@ const getUserData = async function (req, res) {
   let token = req.headers["x-Auth-token"];
   if (!token) token = req.headers["x-auth-token"];
 
-  //If no token is present in the request header return error. This means the user is not logged in.
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
-
   // If a token is present then decode the token with verify function
   // verify takes two inputs:
   // Input 1 is the token to be decoded
   // Input 2 is the same secret with which the token was generated
   // Check the value of the decoded token yourself
-
+ 
   // Decoding requires the secret again. 
   // A token can only be decoded successfully if the same secret was used to create(sign) that token.
   // And because this token is only known to the server, it can be assumed that if a token is decoded at server then this token must have been issued by the same server in past.
@@ -91,8 +87,15 @@ const updateUser = async function (req, res) {
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
   res.send({ status: updatedUser, data: updatedUser });
 };
+const deleteUser=async function(req,res){
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    let update1=await userModel.updateOne({_id:user},{$set:{isDeleted:true}})
+    return res.send({msg:update1})
+}
 
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser=deleteUser
